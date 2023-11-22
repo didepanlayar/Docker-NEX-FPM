d14ce3e337ad878eea17f13384d52916873c9df7
Di Depan Layar <albinmaulana@yahoo.com>
2023-11-22 11:16:29 +0700

    Refactor: Add Color Output

--- logs/clear-log.sh
+++ logs/clear-log.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+Red='\e[1;31m'
+Green='\e[0;32m'
+Yellow='\e[1;33m'
+NC='\e[0m'
 data=(`find logs -name *.log`)
 for log in "${data[@]}"; do
     cat /dev/null > "$log"
-    echo "INFO: Successfully deleted $log"
+    echo -e "${Yellow}INFO${NC}: Successfully deleted ${Red}$log${NC}"
 done
