c0e2f32d1ebec330176257cf39dc4f0727a1d927
Di Depan Layar <albinmaulana@yahoo.com>
2023-12-01 18:03:55 +0700

    Refactor: Nginx Permissions

--- config/config.sh
+++ config/config.sh
@@ -9,5 +9,5 @@ COMPOSER_ALLOW_SUPERUSER=1
 ln -snf /usr/share/zoneinfo/$TIME_ZONE /etc/localtime
 echo $TIME_ZONE > /etc/timezone
 
-chown -R ${PHP_FPM_USER}:${PHP_FPM_GROUP} /var/lib/nginx/html
+chown -R ${PHP_FPM_USER}:${PHP_FPM_GROUP} /var/lib/nginx
 sed -i "s|user nginx;|user ${PHP_FPM_USER};|g" /etc/nginx/nginx.conf
